cd31e89ae2bc17df0bd85bd6d1228e264c7cd35b
rahul-capgemini-testing <rahulcapgeminitesting@gmail.com>
2026-03-29 02:22:58 +0530

    Update LoginPojo.java

--- src/test/java/eComE2E/LoginPojo.java
+++ src/test/java/eComE2E/LoginPojo.java
@@ -1,11 +1,8 @@
 package eComE2E;
 
 public class LoginPojo {
-<<<<<<< HEAD
 //This line is inserted in the next class
-=======
 //This is a pojo class for login page
->>>>>>> d5eef51d9e76d68a8307a2d5bdb90189a7718eb9
 	private String userEmail;
 	private String userPassword;
 	public String getUserEmail() {
